5f19e01c3d63ca2e9a169ac40e51b95ffc9c5b40
Your NamePratham <pra172005@gmail.com>
2025-06-13 03:29:05 +0530

    responsive landing

--- educase_app/src/pages/Landing.jsx
+++ educase_app/src/pages/Landing.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Landing = () => {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center p-1'>
-      <div className='bg-gray-50 border-gray-200 border-2 rounded-md min-h-screen w-fit p-5 flex flex-col items-start justify-end gap-1 shadow-gray-300 shadow-lg'>
-        <div className='text-2xl font-bold'>
+    <div className='min-h-screen w-full flex items-center justify-center p-2 sm:p-4 md:p-8'>
+      
+      <div className='bg-gray-50 border-gray-200 border-2 rounded-md min-h-screen sm:min-h-[90vh] w-full
+         max-w-sm p-5 flex flex-col items-start justify-end gap-2 shadow-gray-300 shadow-lg'>
+      
+        <div className='text-2xl sm:text-2xl font-bold'>
           <h1>Welcome To PopX</h1>
         </div>
 
-        <div className='text-md font-medium text-gray-400 flex flex-col mb-6'>
+        <div className='text-sm sm:text-md font-medium text-gray-400 flex flex-col mb-5'>
           <p>Lorem ipsum, dolor sit amet</p>
           <p>consectetur adipisicing elit</p>
         </div>
 
-        <div className='flex flex-col gap-2'>
-          <button className='px-10 py-2 bg-purple-600 rounded-lg text-white font-mono cursor-pointer'>Create Account</button>
-          <button className='px-10 py-2 bg-purple-200 rounded-lg text-black font-bold cursor-pointer'>Already Registered? Login</button>
+        <div className='flex flex-col gap-3 w-full'>
+          <button className='w-full px-4 py-2 bg-purple-600 rounded-lg text-white font-normal cursor-pointer'>
+            Create Account
+          </button>
+          <button className='w-full px-4 py-2 bg-purple-200 rounded-lg text-black font-bold cursor-pointer'>
+            Already Registered? Login
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
